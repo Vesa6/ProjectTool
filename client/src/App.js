d1802f5de944c0c6319222ac38140832c41e219e
Vesa6 <83303692+Vesa6@users.noreply.github.com>
2024-02-16 21:50:24 +0200

    init Express, add Login handles (#1)
    To run the project, npm install + npm start both the server and the client separately.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Mainpage from './components/Mainpage';
 import './main.css';
 
-// Put whatever you want to render when the app opens inside this div.
+// Client entrypoint.
 const app = () => {
   return (
     <div className="App">
