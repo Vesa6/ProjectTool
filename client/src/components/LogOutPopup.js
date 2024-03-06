4a46d994b319b3435d5c863b57d2398fc911dc93
Jani <jani.alasirnio@tutanota.com>
2024-03-06 14:54:44 +0200

    minor tweak

--- client/src/components/LogOutPopup.js
+++ client/src/components/LogOutPopup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const LogOutPopup = ({ onClose }) => {
   const handleSubmit = async (event) => {
@@ -23,12 +24,14 @@ const LogOutPopup = ({ onClose }) => {
         <h1 className="text-white text-2xl pt-3 pb-5">Log out</h1>
         <p className=" text-sm mb-3">Are you sure you want to log out?</p>
         <div className=" flex flex-row-reverse w-full">
-          <button
-            type="submit"
-            className=" bg-rose-800 px-4 py-2 rounded hover:bg-rose-700 w-fit"
-          >
-            Logout
-          </button>
+          <Link to="/login">
+            <button
+              type="submit"
+              className=" bg-rose-800 px-4 py-2 rounded hover:bg-rose-700 w-fit"
+            >
+              Logout
+            </button>
+          </Link>
           <button className="mr-3" onClick={onClose}>
             Cancel
           </button>
